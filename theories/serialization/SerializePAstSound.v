3161ecf682efe7759d8cd9bc57b9bcde9e8c01ee
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-01-19 02:31:01 +0100

    Prove PAst soundness

---
+++ theories/serialization/SerializePAstSound.v
@@ -0,0 +1,44 @@
+From MetaRocq.Utils Require Import bytestring.
+From Peregrine Require Import PAst.
+From Peregrine Require Import SerializePAst.
+From Peregrine Require Import SerializeEAst.
+From Peregrine Require Import SerializeExAst.
+From Peregrine Require Import SerializeEAstSound.
+From Peregrine Require Import SerializeExAstSound.
+From Peregrine Require Import CeresExtra.
+From Stdlib Require Import List.
+From Ceres Require Import Ceres.
+
+Import ListNotations.
+
+
+
+(* Soundness *)
+
+Instance Sound_typed_env : SoundClass typed_env.
+Proof. typeclasses eauto. Qed.
+
+Instance Sound_untyped_env : SoundClass untyped_env.
+Proof. typeclasses eauto. Qed.
+
+Instance Sound_PAst : SoundClass PAst.
+Proof.
+  unfold SoundClass, Sound.
+  intros l e n He.
+  apply sound_match_con in He.
+  destruct He as [He | He]; elim_Exists He.
+  - destruct He as [es [<- He]].
+    sound_field He.
+    apply sound_class in Ea1.
+    apply sound_class in Ea0.
+    cbn.
+    rewrite <- Ea0, <- Ea1.
+    reflexivity.
+  - destruct He as [es [<- He]].
+    sound_field He.
+    apply sound_class in Ea1.
+    apply sound_class in Ea0.
+    cbn.
+    rewrite <- Ea0, <- Ea1.
+    reflexivity.
+Qed.
