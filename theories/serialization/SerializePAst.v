a62966cc7a137ec3b2fb893677a864da4b0bd701
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-01-19 02:30:25 +0100

    PAst deserializers

--- theories/serialization/SerializePAst.v
+++ theories/serialization/SerializePAst.v
@@ -26,3 +26,31 @@ Instance Serialize_PAst : Serialize PAst :=
     | Untyped env t => [ Atom "Untyped"; to_sexp env; to_sexp t ]
     | Typed env t => [ Atom "Typed"; to_sexp env; to_sexp t ]
     end%sexp.
+
+
+
+(** * Deserializers *)
+
+Instance Deserialize_typed_env : Deserialize typed_env :=
+ fun l e =>
+    _from_sexp l e.
+
+Instance Deserialize_untyped_env : Deserialize untyped_env :=
+ fun l e =>
+    _from_sexp l e.
+
+Instance Deserialize_PAst : Deserialize PAst :=
+  fun l e =>
+    Deser.match_con "PAst"
+      []
+      [ ("Untyped", Deser.con2_ Untyped)
+      ; ("Typed", Deser.con2_ Typed)
+      ]
+      l e.
+
+
+Definition string_of_PAst (ast : PAst) : string :=
+  @to_string PAst Serialize_PAst ast.
+
+Definition PAst_of_string (s : string) : error + PAst :=
+  @from_string PAst Deserialize_PAst s.
