b00c35d916dba62ef9f40f83b5a78ff04eba1e06
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-02-05 10:55:48 +0100

    WIP

--- gen.py
+++ gen.py
@@ -5,11 +5,11 @@ def pp_codepoint(c):
     return f"(D{c[0]},(D{c[1]},(D{c[2]},(D{c[3]},(D{c[4]},D{c[5]})))))"
 
 def pp_compare(r):
-    return f"compare_range c {pp_codepoint(r[0])} {pp_codepoint(r[0])}"
+    return f"compare_range c {pp_codepoint(r[0])} {pp_codepoint(r[1])}"
 
 def pp_match(lines):
     if len(lines) == 1:
-        return f"match {pp_compare(lines[0])} with | Eq => true | Gt => false | Lt => false end"
+        return f"(match {pp_compare(lines[0])} with | Eq => true | Gt => false | Lt => false end)"
     elif len(lines) == 0:
         return "false"
     else:
@@ -17,7 +17,7 @@ def pp_match(lines):
         r = lines[n]
         low = lines[0:n]
         high = lines[n+1:]
-    return f"match {pp_compare(r)} with\n| Eq => true\n| Gt => {pp_match(low)}\n| Lt => {pp_match(high)}\nend"
+    return f"(match {pp_compare(r)} with\n| Eq => true\n| Gt => {pp_match(low)}\n| Lt => {pp_match(high)}\nend)"
 
 
 
