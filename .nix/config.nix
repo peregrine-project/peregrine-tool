32b1aed3fb7a46b5cf9ef88c32c354c364d7936d
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-13 01:24:30 +0100

    Update typed-extraction to version 0.2.1

--- .nix/config.nix
+++ .nix/config.nix
@@ -13,7 +13,7 @@
   bundles."9.1" = { coqPackages = {
       coq.override.version = "9.1";
       metarocq.override.version = "1.4.1-9.1";
-      TypedExtraction.override.version = "0.2.0";
+      TypedExtraction.override.version = "0.2.1";
       CertiCoq.override.version = "coq-9.1";
       verified-extraction.override.version = "rocq-9.1-bs";
       ceres-bs.override.version = "1.0.0";
